4455b75b26af1c8b78cb270bd2da17004505505e
李可凡 <cherish@likefandeMacBook-Pro.local>
2019-11-13 20:41:25 +0800

    inner

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -5,7 +5,7 @@ module.exports = {
       host: '49.232.58.207',
       port: 22,
       ref: 'origin/master',
-      repo: 'https://github.com/cherish553/adminBlog.git',
+      repo: 'git@github.com:cherish553/adminBlog.git',
       path: '/root/blob/adminBlog',
       ssh_options: 'StrictHostKeyChecking=no',
       'pre-deploy': 'git fetch --all',
