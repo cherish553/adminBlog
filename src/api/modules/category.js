d9e877219705f195217552dd7d3320d15d6c58fa
smzdm <smzdm@likefandeMacBook-Pro.local>
2020-05-22 11:41:39 +0800

    修复分类bug

--- src/api/modules/category.js
+++ src/api/modules/category.js
@@ -3,14 +3,14 @@ const pre = '/category'
 export default {
   // 添加文章类别
   add: data =>
-    http.post(`/add`, data),
+    http.post(`${pre}/add`, data),
   // 获取文章列表
   list: ({ page, size, name }) =>
-    http.post(`/list/${page}/${size}`, { name }),
+    http.post(`${pre}/list/${page}/${size}`, { name }),
   // 更新文章数据
-  update: data => http.put(`/update`, data),
+  update: data => http.put(`${pre}/update`, data),
   // 删除文章类别
-  del: id => http.delete(`/del/${id}`),
+  del: id => http.delete(`${pre}/del/${id}`),
   // 获取所有文章类别
-  all: _ => http.get(`/all`)
+  all: _ => http.get(`${pre}/all`)
 }
